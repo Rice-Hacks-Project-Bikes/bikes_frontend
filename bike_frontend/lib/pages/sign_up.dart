23cbfbab8b362cd7d1b52dddadad0949a4f4bf77
Henrymarks1 <henryesmarks@gmail.com>
2022-09-24 01:11:46 -0500

    Adding Web Browsing

---
+++ bike_frontend/lib/pages/sign_up.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sign Up for App"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: _launchUrl, child: Text("Signin with SSO")),
+        ));
+  }
+}
+
+final Uri _url = Uri.parse('https://flutter.dev');
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
+  }
+}
