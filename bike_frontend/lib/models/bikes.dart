d584b3ab76910c9b1bb089bc991ac910e08f1d77
Henrymarks1 <henryesmarks@gmail.com>
2022-09-24 14:15:01 -0500

    Bike stuff

--- bike_frontend/lib/models/bikes.dart
+++ bike_frontend/lib/models/bikes.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Bike {
   String make;
   Position location;
@@ -11,6 +13,24 @@ class Bike {
       required this.image,
       required this.hourlyRate,
       required this.inUse});
+
+  Color inUseColor() {
+    return inUse ? Colors.grey : Colors.green;
+  }
+
+  Widget bikeListTile() {
+    return ListTile(
+      leading: Image.network(image),
+      title: Text(make),
+      subtitle: Text(hourlyRate.toString()),
+      trailing: Container(
+        decoration: BoxDecoration(
+            color: inUseColor(), borderRadius: BorderRadius.circular(20)),
+        height: 20,
+        width: 20,
+      ),
+    );
+  }
 }
 
 class Position {
